760c22d1b322de6547e1ae57e15b66fdef47fd0a
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-23 14:13:20 +0300

    start creating timer

--- js/main.js
+++ js/main.js
@@ -1,79 +1,39 @@
-const tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    tabsParent = document.querySelector(".tabheader__items");
+// Tabs
+let tabcontent = document.querySelectorAll(".tabcontent"),
+    tabs = document.querySelectorAll('.tabheader__item'),
+    tabcontainer = document.querySelector('.tabheader__items');
 
-function hideTabContent() {
-    tabsContent.forEach(item => {
-        item.classList.add('hide')
-        item.classList.remove('show', 'fade')
-    })
-
+function hidetabs() {
+    tabcontent.forEach(item => {
+        item.classList.add("hide")
+        item.classList.remove('show', "fade")
+    });
     tabs.forEach(item => {
-        item.classList.remove("tabheader__item_active")
+        item.classList.remove('tabheader__item_active')
     })
 }
-
-// soni i = 0 / function es6
-function showTabContent(i = 0) {
-    tabsContent[i].classList.add('show', 'fade')
-    tabsContent[i].classList.remove('hide')
-    tabs[i].classList.add('tabheader__item_active')
+function showtabs(i) {
+    tabcontent[i].classList.add("show", "fade")
+    tabcontent[i].classList.remove("hide")
+    tabs[i].classList.add("tabheader__item_active")
 }
+hidetabs()
+showtabs(1)
 
-hideTabContent()
-showTabContent()
+tabcontainer.addEventListener("click", (e) => {
+    let target = e.target;
 
-// function for show tab onclick 
-tabsParent.addEventListener("click", (event) => {
-    const target = event.target;
-
-    if (target && target.classList.contains('tabheader__item')) {
+    if (target && target.classList.contains("tabheader__item")) {
         tabs.forEach((item, index) => {
-            if (target == item) {
-                hideTabContent()
-                showTabContent(index)
+            if (target === item) {
+                hidetabs();
+                showtabs(index);
             }
         })
     }
 })
 
+// Timer 
 
-// Modal
+const deadline = '2023-10-10'
 
-const modalTrigger = document.querySelectorAll('[data-modal]'),
-    modal = document.querySelector(".modal"),
-    modalCloseBtn = document.querySelector("[data-close]");
-
-modalTrigger.forEach(btn => {
-    btn.addEventListener('click', () => {
-        modal.classList.add("show")
-        modal.classList.remove("hide")
-        // after 
-        document.body.style.overflow = "hidden"
-        // modal.classList.toggle("show")
-    })
-})
-
-modalCloseBtn.addEventListener('click', () => {
-    modal.classList.add("hide")
-    modal.classList.remove("show")
-    // after 
-    document.body.style.overflow = ""
-    // modal.classList.toggle("show")
-})
-
-modal.addEventListener('click', (e) => {
-    if (e.target == modal) {
-        modal.classList.add("hide")
-        modal.classList.remove("show")
-        document.body.style.overflow = ""
-    }
-})
-
-document.addEventListener('keydown', (e) => {
-    if (e.code === "Escape" && modal.classList.contains("show")) {
-        modal.classList.add("hide")
-        modal.classList.remove("show")
-        document.body.style.overflow = ""
-    }
-})
