3878cdf92dae719f0d6f3234da9fea902d0b9bfd
Davlatshoh6275 <davlatshoh6275@gmail.com>
2024-02-16 13:28:51 +0300

    add slider function

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ function hidetabs() {
     item.classList.remove("tabheader__item_active");
   });
 }
+
 function showtabs(i) {
   tabcontent[i].classList.add("show", "fade");
   tabcontent[i].classList.remove("hide");
@@ -97,33 +98,81 @@ setClock(".timer", deadline);
 
 // Slider
 
+// const slides = document.querySelectorAll(".offer__slide"),
+//   prev = document.querySelector(".offer__slider-prev"),
+//   next = document.querySelector(".offer__slider-next"),
+//   total = document.querySelector("#total"),
+//   current = document.querySelector("#current"),
+//   slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+//   slider = document.querySelector(".offer__slider"),
+//   slidesField = document.querySelector(".offer__slider-inner"),
+//   width = window.getComputedStyle(slidesWrapper).width;
+
+// let slideIndex = 1;
+// let offset = 0;
+
+// if (slides.length < 10) {
+//   total.textContent = `0${slides.length}`;
+//   current.textContent = `0${slideIndex}`;
+// } else {
+//   total.textContent = slides.length;
+//   current.textContent = slideIndex;
+// }
+
+// slidesField.style.width = 100 * slides.length + "%";
+// slidesField.style.display = "flex";
+// slidesField.style.transition = "0.5s all linear";
+
+// slidesWrapper.style.overflow = "hidden";
+
+// slides.forEach((slide) => {
+//   slide.style.width = width;
+// });
+
+// Slider
+
+let slideIndex = 1;
 const slides = document.querySelectorAll(".offer__slide"),
   prev = document.querySelector(".offer__slider-prev"),
   next = document.querySelector(".offer__slider-next"),
   total = document.querySelector("#total"),
-  current = document.querySelector("#current"),
-  slidesWrapper = document.querySelector(".offer__slider-wrapper"),
-  slider = document.querySelector(".offer__slider"),
-  slidesField = document.querySelector(".offer__slider-inner"),
-  width = window.getComputedStyle(slidesWrapper).width;
+  current = document.querySelector("#current");
 
-let slideIndex = 1;
-let offset = 0;
+showSlides(slideIndex);
 
 if (slides.length < 10) {
   total.textContent = `0${slides.length}`;
-  current.textContent = `0${slideIndex}`;
 } else {
   total.textContent = slides.length;
-  current.textContent = slideIndex;
 }
 
-slidesField.style.width = 100 * slides.length + "%";
-slidesField.style.display = "flex";
-slidesField.style.transition = "0.5s all linear";
+function showSlides(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-slidesWrapper.style.overflow = "hidden"
+  slides.forEach((item) => (item.style.display = "none"));
 
-slides.forEach((slide) => {
-  slide.style.width = width;
+  slides[slideIndex - 1].style.display = "block"; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+
+  if (slides.length < 10) {
+    current.textContent = `0${slideIndex}`;
+  } else {
+    current.textContent = slideIndex;
+  }
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+prev.addEventListener("click", function () {
+  plusSlides(-1);
+});
+
+next.addEventListener("click", function () {
+  plusSlides(1);
 });
